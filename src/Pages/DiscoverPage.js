6ddb74a5e59f56a967b82060b6a8e31ac38b4ad1
Vinnie <vinnie.terlouw@gmail.com>
2022-01-26 12:10:17 +0100

    added pages

---
+++ src/Pages/DiscoverPage.js
@@ -0,0 +1,3 @@
+export default function DiscoverPage() {
+  return <p> This is the discover page</p>;
+}
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <p> This is the home page </p>;
+}
